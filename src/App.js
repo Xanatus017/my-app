97221f250b09cae226c0f657fea37eed6e1abe66
JASPER\danes <danjasper.de@gmail.com>
2024-12-18 12:20:01 -0600

    set up to start the quiz and timer

--- src/App.js
+++ src/App.js
@@ -7,6 +7,22 @@ const App = () => {
   const [difficulty, setDifficulty] = useState("");
   const [timePerQuestion, setTimePerQuestion] = useState(30);
   const [questions, setQuestions] = useState([]);
+  const [quizStarted, setQuizStarted] = useState(false);
 }
+
+useEffect(() => {
+  if (quizStarted && timeLeft > 0) {
+    const timer = setInterval(() => setTimeLeft((prev) => prev - 1), 1000);
+    return () => clearInterval(timer);
+  }
+  if (timeLeft === 0) handleNext();
+}, [timeLeft, quizStarted]);
+
+
+const startQuiz = () => {
+  setQuizStarted(true);
+  fetchQuestions();
+};
+
 
 export default App;
