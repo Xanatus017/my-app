b7189d4eae7c9605ad38a493f58acf5c96531279
JASPER\danes <danjasper.de@gmail.com>
2024-12-18 12:06:36 -0600

    set up quiz app - questions, category, difficulty, timer and questions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 
-
+const App = () => {
+  const [numQuestions, setNumQuestions] = useState(5);
+  const [category, setCategory] = useState("");
+  const [difficulty, setDifficulty] = useState("");
+  const [timePerQuestion, setTimePerQuestion] = useState(30);
+  const [questions, setQuestions] = useState([]);
+}
 
 export default App;
